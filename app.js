b2c6460b00f0de06559f410e7973c6ae1801d161
Daniel Tooke <daniel.tooke@gmail.com>
2018-04-24 16:21:11 +0100

    FIXED EVERYTHING

--- app.js
+++ app.js
@@ -50,38 +50,7 @@ router.get("/",function(req,res){
   res.sendFile(__dirname + "/index.html");
 });
 
-router.route("/properties")
-  .get(function(req,res){
-      var response = {};
-      mongoOp.find({},function(err,data){
-      // Mongo command to fetch all data from collection.
-          if(err) {
-              response = {"error" : true,"message" : "Error fetching data"};
-          } else {
-              response = {"error" : false,"message" : data};
-          }
-          res.json(response);
-      });
-  })
-  .post(function(req,res){
-    var db = new mongoOp();
-    var response = {};
-    // fetch data from REST request.
-    // Add strict validation when you use this in Production.
-    db.propertyName = req.body.propertyName;
-    db.propertyDetails = req.body.propertyDetails;
-    db.propertyPrice = req.body.propertyPrice;
-    db.save(function(err){
-    // save() will run insert() command of MongoDB.
-    // it will add new data in collection.
-        if(err) {
-            response = {"error" : true,"message" : "Error adding data"};
-        } else {
-            response = {"error" : false,"message" : "Data added"};
-        }
-        res.json(response);
-      });
-});
+
 
 app.use('/',router);
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var mongoOp     =   require("../model/mongo");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,6 +17,39 @@ router.get('/property/new', function(req, res, next) {
 
 router.post('/property/new', function(req, res, next) {
   res.render('property', { title: 'Makers BnB' });
+});
+
+router.route("/properties")
+  .get(function(req,res){
+      var response = {};
+      mongoOp.find({},function(err,data){
+      // Mongo command to fetch all data from collection.
+          if(err) {
+              response = {"error" : true,"message" : "Error fetching data"};
+          } else {
+              response = {"error" : false,"message" : data};
+          }
+          res.json(response);
+      });
+  })
+  .post(function(req,res){
+    var db = new mongoOp();
+    var response = {};
+    // fetch data from REST request.
+    // Add strict validation when you use this in Production.
+    db.propertyName = req.body.propertyName;
+    db.propertyDetails = req.body.propertyDetails;
+    db.propertyPrice = req.body.propertyPrice;
+    db.save(function(err){
+    // save() will run insert() command of MongoDB.
+    // it will add new data in collection.
+        if(err) {
+            response = {"error" : true,"message" : "Error adding data"};
+        } else {
+            response = {"error" : false,"message" : "Data added"};
+        }
+        res.json(response);
+      });
 });
 
 module.exports = router;
