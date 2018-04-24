9e3700930969ca5b84e597db7867cb62739ba07b
Lorraine Philllips <ljcphilllips@gmail.com>
2018-04-24 13:52:26 +0100

    converted html to pug files

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/users/new', function(req, res, next) {
   res.render('sign_up_in', { title: 'Makers BnB' });
 });
 
+router.get('/property/new', function(req, res, next) {
+  res.render('property', { title: 'Makers BnB' });
+});
+
+router.post('/property/new', function(req, res, next) {
+  res.render('property', { title: 'Makers BnB' });
+});
+
 module.exports = router;
