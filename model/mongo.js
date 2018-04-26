618314de25924477a8ea125f540d1c6b29da7fea
Gadiza Zerari <gadizaz@hotmail.com>
2018-04-26 11:19:11 +0100

    Commit mongo.js

---
+++ model/mongo.js
@@ -0,0 +1,12 @@
+var mongoose    =   require("mongoose");
+mongoose.connect(MONGO_URL);
+// create instance of Schema
+var mongoSchema =   mongoose.Schema;
+// create schema
+var propertySchema  = {
+    "propertyName" : String,
+    "propertyDetails" : String,
+    "propertyPrice" : String
+};
+// create model if not exists.
+module.exports = mongoose.model('properties',propertySchema);
