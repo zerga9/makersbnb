e4b1e01623b03fd45387c41c5cd4491067df45c1
Louis Moselhi <louismoselhi@MacBook-Pro-5.local>
2018-04-26 15:53:33 +0100

    added cypress security thing to bypass cross-origin, removed username requirement on sign up page

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -28,11 +28,12 @@ describe('Add property page', function(){
   it('adds property and sees it on properties page', function() {
     cy.visit('http://localhost:3000/property/new');
     cy.request('POST', '/properties', {
-      propertyName: 'House',
+      propertyName: 'Cypress Hill',
       propertyDetails: 'Another great place',
       propertyPrice: '£28 a night'
     });
-    cy.contains('Another great place');
+    cy.contains('Submit').click();
+    cy.contains('£28 a night');
   });
 });
 
